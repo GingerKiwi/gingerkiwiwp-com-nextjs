27786277adf310522878d984a46c6eb0fce4fd1c
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2023-10-23 13:42:28 -0400

    set up next.js project. connect GraphQL for WordPress

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,26 @@
+import { gql } from "@apollo/client";
+import client from "client";
+
 export default function Home() {
   return <div>Next JS &amp; WordPress course.</div>;
+}
+
+export const getStaticProps = async () => {
+  const { data } = await client.query({
+    query: gql`
+    query NewQuery {
+      pages {
+        nodes {
+          title
+        }
+      }
+    }
+    `
+  })
+  return {
+    props: {
+      data,
+      myexampleprops: "testing"
+    }
+  }
 }
